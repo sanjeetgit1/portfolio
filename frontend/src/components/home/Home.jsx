51ca0cd5a5980d328ecee85ecabfd8a502656cd7
sanjeetgit1 <sksanjeet616@gmail.com>
2024-06-09 19:48:52 +0530

    thirdday

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,80 +1,124 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import "./Home.css";
 import * as THREE from "three";
 // import { redirect } from 'react-router-dom';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import moonImage from "../../Images/moon.jpg"
+
+import moonImage from "../../Images/moon.jpg";
+import venusImage from "../../Images/venus.jpg";
+
+const Home = () => {
+  useEffect(() => {
+    const textureloader = new THREE.TextureLoader();
+    const moonTexture = textureloader.load(moonImage);
+    const venusTexture = textureloader.load(venusImage);
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(4, 4, 8);
+    const canvas = document.querySelector(".homeCanvas");
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    // ShereGeometry
+    const moonGemetry = new THREE.SphereGeometry(2, 64, 64);
+    const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture });
+    const moon = new THREE.Mesh(moonGemetry, moonMaterial);
+  
+
+
+    const venusGemetry = new THREE.SphereGeometry(3, 64, 64);
+    const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
+    const venus = new THREE.Mesh(venusGemetry, venusMaterial);
+  venus.position.set(8, 5, 5);
+    // pointLight
+    const pointLight = new THREE.PointLight(0xffffff, 23);
+    const pointLight2 = new THREE.PointLight(0xffffff, 2);
+   
+    scene.add(moon);
+    scene.add(venus); 
+    pointLight.position.set(8,5,5);
+    pointLight2.position.set(-8,-5,-5);
+    scene.add(pointLight);
+    scene.add(pointLight2);
+
+    /*//pointLightHelper
+     const lightHelper = new THREE.PointLightHelper(pointLight);
+     scene.add(lightHelper);
+    */
+
+
+     const constSpeed =0.01;
+     window.addEventListener("mousemove",(e)=>{
+        if(e.clientX < window.innerWidth/2){
+            moon.rotation.x -=constSpeed;
+            moon.rotation.y += constSpeed;
+            venus.rotation.x -=constSpeed;
+            venus.rotation.y += constSpeed;
+
+        }
+        if(e.clientX > window.innerWidth/2){
+            moon.rotation.x -=constSpeed;
+            moon.rotation.y -= constSpeed;
+            venus.rotation.x -=constSpeed;
+            venus.rotation.y -= constSpeed;
+
+        }
+        if(e.clientY > window.innerHeight/2){
+            moon.rotation.x -=constSpeed;
+            moon.rotation.y += constSpeed;
+            venus.rotation.x -=constSpeed;
+            venus.rotation.y += constSpeed;
+
+        }
+         if(e.clientY <= window.innerHeight/2){
+            moon.rotation.x -=constSpeed;
+            moon.rotation.y -= constSpeed;
+            venus.rotation.x -=constSpeed;
+            venus.rotation.y -= constSpeed;
+
+        }
+        
+     });
 
 
 
+ 
 
-const Home=()=> {
-
-
-    useEffect(()=>{
-
-        const textureloader = new THREE.TextureLoader();
-        const moonTexture= textureloader.load(moonImage);
+  
 
 
+    const animate = () => {
+      requestAnimationFrame(animate);
+      // moon.rotation.x +=0.01;
+      // moon.rotation.y +=0.01;
+      moon.rotation.z += 0.01;
+      venus.rotation.z += 0.001;
 
-        const scene = new THREE.Scene();
-        const camera=new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
+      // camera.position.x+=0.01
+      // moon.position.x +=0.01;
+      // moon.position.y +=0.01;
+      // moon.position.z +=0.01;
+      // moon.position.x -=0.02;
+      // moon.position.y -=0.02;
+      // moon.position.z -=0.02;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
 
-        const canvas= document.querySelector('.homeCanvas');
-        const renderer= new THREE.WebGLRenderer({canvas});
-// ShereGeometry
-        const moonGemetry= new THREE.SphereGeometry(3,64,64);
-        const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture });
+  return (
+    <div className="home">
+      <canvas className="homeCanvas"></canvas>
+    </div>
+  );
+};
 
-
-// pointLight
-const pointLight = new THREE.PointLight(0xffffff, 20);
-        pointLight.position.z=7;
-  scene.add(pointLight);
-
-     //pointLightHelper   
-        const lightHelper = new THREE.PointLightHelper(pointLight);
-        const moon = new THREE.Mesh(moonGemetry,moonMaterial);
- scene.add(lightHelper);
-
-
- const controls= new OrbitControls(camera,renderer.domElement);
-
-       scene.add(moon);
-     
-      
-
-       camera.position.z =10;
-       const animate=()=>{
-        requestAnimationFrame(animate);
-        // moon.rotation.x +=0.01;
-        // moon.rotation.y +=0.01;
-        // moon.rotation.z +=0.01;
-        // camera.position.x+=0.01
-        // moon.position.x +=0.01;
-        // moon.position.y +=0.01;
-        // moon.position.z +=0.01;
-        // moon.position.x -=0.02;
-        // moon.position.y -=0.02;
-        // moon.position.z -=0.02;
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.render(scene, camera);
-       };
-       animate();
-
-
-    },[]);
-
-    return (
-        <div className='home'>
-            <canvas className='homeCanvas'></canvas>
-        </div>
-      
-    )
-}
-
-export default Home
+export default Home;
 
 // import React, { useEffect } from 'react';
 // import "./Home.css";
