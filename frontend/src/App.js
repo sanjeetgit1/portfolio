6b97ad53749a7d9c6a022d6fb8c0f597d71a5af0
sanjeetgit1 <sksanjeet616@gmail.com>
2024-06-18 23:53:59 +0530

    create Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import Home from './components/home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <Router>
@@ -10,9 +11,8 @@ function App() {
       <Routes>
 
         <Route path="/" element={<Home/>} />
-     
-
   </Routes>
+  <Footer/>
     </Router>
   
   );
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import "./Footer.css"
+import {Typography} from "@mui/material";
+import React from "react";
+import {Link} from "react-router-dom";
+import {BsGithub,BsYoutube,BsInstagram,BsLinkedin} from "react-icons/bs";
+
+import "./footer.css"
+
+
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div className='footer'>
+
+
+  <div>
+    <Typography variant="h5">About Me</Typography>
+      <Typography>
+ Hello , My name is Sanjeet kushwaha . I am a full-Stack Developer .
+  </Typography>
+  <Link to="/contact" className="footerContactBtn">
+  <Typography>Contact Us</Typography>
+  </Link>
+  </div>
+  <div>
+    <a href="https://github.com/sanjeetgit1" target='black'><BsGithub/></a>
+    <a href="https://www.linkedin.com/in/sanjeet-kushwaha-54905b228/" target='black'><BsLinkedin/></a>
+    <a href="#" target='black'><BsInstagram/></a>
+    <a href="#" target='black'><BsYoutube/></a>
+  </div>
+
+     </div>
+  
 }
 
 export default Footer
